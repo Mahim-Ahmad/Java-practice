e5abd1231b95830834bfe8185dd61772be40869b
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2026-01-04 12:23:56 +0600

    u

--- p1.java
+++ p1.java
@@ -2,6 +2,8 @@ public class p1 {
     public static void main(String[] args) {
         System.out.println("This is p1 class.");
         System.out.println("Hello, World!");
-        System.err.println("This is an error message.");
+        System.out.println("\"This is an error message.\"");
+        System.out.println("\\This is an error message.\\");
+
     }
 }
